a09fd73b4f2d185b6133e5baec4aa5994ba5b134
Rachell Weiss <rachellweiss@macbookpro.lan>
2025-08-27 01:24:08 -0500

    Fix Next.js config: remove invalid experimental options

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  experimental: {
-    excludeDefaultMomentLocales: false,
-  }
 };
 
 export default nextConfig;
